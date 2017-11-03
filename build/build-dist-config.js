b58082405c1680c2e73702cb574dd552adccc31d
wxsm <edisond@qq.com>
2017-11-03 12:55:47 +0800

    disable github page jekyll

--- build/build-dist-config.js
+++ build/build-dist-config.js
@@ -13,3 +13,7 @@ ensureDir()
 
 // write CNAME
 fs.writeFileSync(DIST_PATH + 'CNAME', 'wxsm.space')
+
+// https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/
+// If you add a .nojekyll file, your source files will be published without any modifications.
+fs.writeFileSync(DIST_PATH + '.nojekyll', '')
