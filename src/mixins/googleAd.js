f505574823332de7b5fc3609479a33608f6c5f30
wxsm <wxsms@foxmail.com>
2019-03-08 20:31:50 +0800

    google ad

--- src/mixins/googleAd.js
+++ src/mixins/googleAd.js
@@ -17,11 +17,13 @@ export default {
       return
     }
     appendScript()
-    window.adsbygoogle = window.adsbygoogle ? window.adsbygoogle : []
-    const num = this.$el.querySelectorAll('.adsbygoogle').length
-    // console.log(`found ${num} ad slot.`)
-    for (let i = 0; i < num; ++i) {
-      window.adsbygoogle.push({})
-    }
+    setTimeout(() => {
+      window.adsbygoogle = window.adsbygoogle ? window.adsbygoogle : []
+      const num = this.$el.querySelectorAll('.adsbygoogle').length
+      // console.log(`found ${num} ad slot.`)
+      for (let i = 0; i < num; ++i) {
+        window.adsbygoogle.push({})
+      }
+    }, 500)
   }
 }
