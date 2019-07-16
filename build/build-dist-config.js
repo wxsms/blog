7f900cdf4f867deeddab6268e892f91f16fef693
wxsm <wxsms@foxmail.com>
2019-07-16 15:20:00 +0800

    add ads.txt

--- build/build-dist-config.js
+++ build/build-dist-config.js
@@ -20,3 +20,6 @@ fs.writeFileSync(DIST_PATH + 'baidu_verify_WrnmKQXx4O.html', 'WrnmKQXx4O')
 // https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/
 // If you add a .nojekyll file, your source files will be published without any modifications.
 fs.writeFileSync(DIST_PATH + '.nojekyll', '')
+
+// https://support.google.com/adsense/answer/7532444?hl=zh-Hans
+fs.writeFileSync(DIST_PATH + 'ads.txt', 'google.com, pub-4714899946256166, DIRECT, f08c47fec0942fa0')
