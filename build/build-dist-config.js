a3b1d3e6e21bfe18cb303fd8f2e619c154ef8345
wxsm <wxsms@foxmail.com>
2019-07-22 14:50:42 +0800

    blog rebuild

--- build/build-dist-config.js
+++ build/build-dist-config.js
@@ -1,15 +1,6 @@
 const fs = require('fs')
-const DIST_PATH = './dist/'
-
-const ensureDir = () => {
-  try {
-    fs.accessSync(DIST_PATH, fs.F_OK)
-  } catch (e) {
-    fs.mkdirSync(DIST_PATH)
-  }
-}
-
-ensureDir()
+const path = require('path')
+const DIST_PATH = path.join(__dirname, '../src/.vuepress/dist/')
 
 // write CNAME
 fs.writeFileSync(DIST_PATH + 'CNAME', 'wxsm.space')
