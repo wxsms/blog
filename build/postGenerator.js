484cdb42eacb1be2e755cca352ed07a998dc5163
wxsm <wxsms@foxmail.com>
2020-10-12 13:21:07 +0800

    feat: auto folder names & allow select

--- build/postGenerator.js
+++ build/postGenerator.js
@@ -26,12 +26,30 @@ function titleToSlug (str) {
   return str
 }
 
+/**
+ * get all folder names from a directory
+ * https://stackoverflow.com/questions/18112204/get-all-directories-within-directory-nodejs
+ * @param source
+ * @returns {[string]}
+ */
+function getDirectories (source) {
+  return fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter(v => v.isDirectory())
+    .map(v => v.name)
+}
+
 (async () => {
-  const str = fs.readFileSync(path.join(__dirname, 'server-views/post-template.md'), 'utf8')
+  const postsPath = path.join(__dirname, '..', 'src', 'posts')
+  const folders = getDirectories(postsPath)
+  const str = fs.readFileSync(path.join(__dirname, 'templates', 'post-template.md'), 'utf8')
   const postTitle = read.question('Enter post title: ') || ''
-// const postId = read.question('Enter post id: ')
   const tags = read.question('Enter tags (separate by comma): ') || ''
-  const folder = read.question('Which folder (tech / life, etc.): ') || ''
+  const folderIndex = read.keyInSelect(folders, 'Which folder?')
+  if (folderIndex === -1) {
+    return
+  }
+  const folder = folders[folderIndex]
   const isoDateStr = new Date().toISOString()
   const slug = `${isoDateStr.substr(0, 10)}-${titleToSlug(postTitle)}`
   const filename = `${slug}.md`
@@ -43,7 +61,7 @@ function titleToSlug (str) {
     .replace('{{tags}}', tags)
     .replace('{{date}}', isoDateStr)
 
-  const filePath = path.join(__dirname, '..', 'src', 'posts', folder, filename)
+  const filePath = path.join(postsPath, folder, filename)
   fs.writeFileSync(filePath, file)
   await git.add(filePath)
   console.log(`${filename} created.`)
