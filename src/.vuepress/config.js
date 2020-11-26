99f78ff1271808eb2b35a7b435a3017c826cdc00
wxsm <wxsms@foxmail.com>
2020-11-26 09:36:48 +0800

    refactor: remove useless deps

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,23 +1,15 @@
 const _ = require('lodash')
 
 module.exports = {
-  // shouldPrefetch: () => false,
   title: `wxsm's space`,
   description: 'Just another personal blog.',
   head: [
     ['meta', { name: 'google-site-verification', content: 'ekuL5J7xK1IdFtP13v3KxpuGKnYS1oCT9PvZdjYm8Eg' }],
     ['link', { rel: 'shortcut icon', type: 'image/png', href: 'favicon.png' }],
     ['link', { rel: 'apple-touch-icon', size: '200x200', href: 'favicon-iphone.png' }]
-    // ['script', {
-    //   'data-ad-client': 'ca-pub-4714899946256166',
-    //   async: true,
-    //   src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-    // }]
   ],
   theme: 'mini',
   themeConfig: {
-    // useSimpleLinkOnNavBar: true,
-    // 这个平滑滚动是通过 css 实现的，会导致一些不必要的动画，较为烦人
     smoothScroll: false,
     lastUpdated: 'Last Updated',
     siteName: 'wxsm\'s space',
@@ -53,21 +45,8 @@ module.exports = {
       posts_directories: ['/posts'],
       sort: entries => _.reverse(_.sortBy(entries, 'date'))
     }
-    // 'vuepress-plugin-serve': {},
-    // 'vuepress-plugin-dehydrate': {
-    //   // disable SSR
-    //   noSSR: '404.html',
-    //   // remove scripts
-    //   noScript: [
-    //     // '**/2018-07-19-gitlab-ci-setup.html'
-    //   ]
-    // }
   },
   markdown: {
-    // toc: {
-    //   containerHeaderHtml: '<p>目录：</p>',
-    //   containerFooterHtml: '<br/>'
-    // },
     lineNumbers: false,
     extendMarkdown: md => {
       md.use(require('markdown-it-imsize'))
