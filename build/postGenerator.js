f468a759546be07c6ae61949ac5131976be187dc
guokairui <guokairui1@wps.cn>
2021-08-24 15:44:38 +0800

    docs: update post

--- build/postGenerator.js
+++ build/postGenerator.js
@@ -17,29 +17,31 @@ function getDirectories (source) {
 }
 
 (async () => {
-  const postsPath = path.join(__dirname, '..', 'src', 'posts')
-  const folders = getDirectories(postsPath)
+  const now = new Date()
+  const year = now.getFullYear().toString()
+
+  const postsPath = path.join(__dirname, '..', 'src', year)
+  if (!fs.existsSync(postsPath)) {
+    fs.mkdirSync(postsPath)
+  }
+
   const str = fs.readFileSync(path.join(__dirname, 'templates', 'post-template.md'), 'utf8')
   const postTitle = read.question('Enter post title: ') || ''
   const postId = read.question('Enter post id: ') || ''
   const tags = read.question('Enter tags (separate by comma): ') || ''
-  const folderIndex = read.keyInSelect(folders, 'Which folder?')
-  if (folderIndex === -1) {
-    return
-  }
-  const folder = folders[folderIndex]
+
   const isoDateStr = new Date().toISOString()
-  const slug = `${isoDateStr.substr(0, 10)}-${postId}`
-  const filename = `${slug}.md`
-  const permalink = `/posts/${slug}.html`
+  const slug = postsPath + '/' + postId
+  const filename = `index.md`
+
+  fs.mkdirSync(slug)
 
   const file = str
-    .replace('{{permalink}}', permalink)
     .replace('{{title}}', postTitle)
     .replace('{{tags}}', tags)
     .replace('{{date}}', isoDateStr)
 
-  const filePath = path.join(postsPath, folder, filename)
+  const filePath = path.join(slug, filename)
   fs.writeFileSync(filePath, file)
   await git.add(filePath)
   console.log(`${filename} created.`)
--- build/templates/post-template.md
+++ build/templates/post-template.md
@@ -1,5 +1,4 @@
 ---
-permalink: '{{permalink}}'
 title: '{{title}}'
 date: {{date}}
 tags: [{{tags}}]
