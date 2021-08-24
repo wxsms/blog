5b47bb0ff73a2e21eb32c80059a204203c978245
guokairui <guokairui1@wps.cn>
2021-08-24 15:46:21 +0800

    docs: update post

--- build/postGenerator.js
+++ build/postGenerator.js
@@ -1,7 +1,7 @@
-const read = require('readline-sync')
-const fs = require('fs')
-const path = require('path')
-const git = require('simple-git')()
+const read = require('readline-sync');
+const fs = require('fs');
+const path = require('path');
+const git = require('simple-git')();
 
 /**
  * get all folder names from a directory
@@ -13,36 +13,36 @@ function getDirectories (source) {
   return fs
     .readdirSync(source, { withFileTypes: true })
     .filter(v => v.isDirectory())
-    .map(v => v.name)
+    .map(v => v.name);
 }
 
 (async () => {
-  const now = new Date()
-  const year = now.getFullYear().toString()
+  const now = new Date();
+  const year = now.getFullYear().toString();
 
-  const postsPath = path.join(__dirname, '..', 'src', year)
+  const postsPath = path.join(__dirname, '..', 'src', year);
   if (!fs.existsSync(postsPath)) {
-    fs.mkdirSync(postsPath)
+    fs.mkdirSync(postsPath);
   }
 
-  const str = fs.readFileSync(path.join(__dirname, 'templates', 'post-template.md'), 'utf8')
-  const postTitle = read.question('Enter post title: ') || ''
-  const postId = read.question('Enter post id: ') || ''
-  const tags = read.question('Enter tags (separate by comma): ') || ''
+  const str = fs.readFileSync(path.join(__dirname, 'templates', 'post-template.md'), 'utf8');
+  const postTitle = read.question('Enter post title: ') || '';
+  const postId = read.question('Enter post id: ') || '';
+  const tags = read.question('Enter tags (separate by comma): ') || '';
 
-  const isoDateStr = new Date().toISOString()
-  const slug = postsPath + '/' + postId
-  const filename = `index.md`
+  const isoDateStr = new Date().toISOString();
+  const slug = path.join(postsPath, postId);
+  const filename = `index.md`;
 
-  fs.mkdirSync(slug)
+  fs.mkdirSync(slug);
 
   const file = str
     .replace('{{title}}', postTitle)
     .replace('{{tags}}', tags)
-    .replace('{{date}}', isoDateStr)
+    .replace('{{date}}', isoDateStr);
 
-  const filePath = path.join(slug, filename)
-  fs.writeFileSync(filePath, file)
-  await git.add(filePath)
-  console.log(`${filename} created.`)
-})()
+  const filePath = path.join(slug, filename);
+  fs.writeFileSync(filePath, file);
+  await git.add(filePath);
+  console.log(`${filename} created.`);
+})();
