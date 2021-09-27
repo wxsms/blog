ee2b77a44dfc112440cd82aabe1f1a6c0c54c588
guokairui <guokairui1@wps.cn>
2021-09-27 10:47:42 +0800

    pwa

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
     ['link', { rel: 'apple-touch-icon', size: '200x200', href: '/favicon-iphone.png' }],
     ['link', { rel: 'icon', href: '/favicon-iphone.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['link', { rel: 'apple-touch-icon', href: '/favicon-iphone.png' }],
