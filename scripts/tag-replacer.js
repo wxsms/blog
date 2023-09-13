5362f38308c24d8e0c3e17ce110ea1364b0b9d10
guokairui <wxsms@foxmail.com>
2023-09-13 17:21:37 +0800

    u

--- scripts/tag-replacer.js
+++ scripts/tag-replacer.js
@@ -3,19 +3,10 @@
 hexo.extend.filter.register('before_post_render', function (data) {
   // vuepress style tips
   data.content = data.content
-    .replace(/::: tip/g, '{% note info %}')
-    .replace(/:::tip/g, '{% note info %}')
-    .replace(/::: info/g, '{% note info %}')
-    .replace(/:::info/g, '{% note info %}')
-    .replace(/::: warning/g, '{% note warning %}')
-    .replace(/:::warning/g, '{% note warning %}')
-    .replace(/::: warn/g, '{% note warning %}')
-    .replace(/:::warn/g, '{% note warning %}')
-    .replace(/::: danger/g, '{% note danger %}')
-    .replace(/:::danger/g, '{% note danger %}')
-    .replace(/::: error/g, '{% note danger %}')
-    .replace(/:::error/g, '{% note danger %}')
-    .replace(/:::/g, '{% endnote %}');
+    .replace(/:::\s?(tip|info)/ig, '{% note info %}')
+    .replace(/:::\s?(warning|warn)/ig, '{% note warning %}')
+    .replace(/:::\s?(danger|error)/ig, '{% note danger %}')
+    .replace(/:::/ig, '{% endnote %}');
 
   return data;
 });
